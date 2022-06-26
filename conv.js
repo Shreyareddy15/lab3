26032d41223ac00b9800dee3fa360fe3151058b7
shreya-schandup <107084633+shreya-schandup@users.noreply.github.com>
2022-06-26 11:12:51 -0500

    Add files via upload

---
+++ conv.js
@@ -0,0 +1,10 @@
+
+function kgtopound(val) {
+    document.getElementById("poundval").innerHTML = val * 2.2046
+}
+function kmtomiles(val) {
+    document.getElementById("milesval").innerHTML = val * 0.62137
+}
+function ftoc(val) {
+    document.getElementById("celcius").innerHTML = (parseFloat(val) - 32) / 1.8
+}
